7d0290d21b98f906c051907db3294dec70893c7d
Jhon_lopera_desktop <jhon.lopera05rep@gmail.com>
2022-03-13 19:51:00 -0500

    Show propierties on admin page from database in mysql

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,53 @@
 <?php
-    require '../includes/functions.php';
-    include_template("header");
+
+require '../includes/config/database.php';
+$db = connect_db(); // connect to db
+$query = "SELECT * FROM propiedades"; //define query
+$queryResult = mysqli_query($db, $query);
+
+$result = $_GET["result"] ?? null; //if result does not exit, asign null
+
+require '../includes/functions.php';
+include_template("header");
 ?>
 
 <main class="contenedor seccion">
     <h1>Administrador Bienes Raices</h1>
+    <?php if ($result == 1) : ?>
+        <p class="alerta exito">Información almacenada Correctamente</p>
+    <?php endif; ?>
     <a href="/admin/propierties/create.php" class="boton-verde">Nueva Propiedad</a>
+
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php while ($row = mysqli_fetch_assoc($queryResult)) : ?> <!-- Show results -->
+                <tr>
+                    <td><?php echo $row["id"]; ?></td>
+                    <td><?php echo $row["titulo"]; ?></td>
+                    <td> <img src="/build/imgPropierties/<?php echo $row["imagen"]; ?>" alt="imagen_propiedad"> </td>
+                    <td>$<?php echo $row["precio"]; ?></td>
+                    <td>
+                        <a class="boton-verde-block" href="#">Eliminar</a>
+                        <a class="boton-amarillo-block" href="#">Actualizar</a>
+                    </td>
+                </tr>
+            <?php endwhile ?>
+        </tbody>
+    </table>
+
 </main>
 
-<?php include_template("footer"); ?>
+<?php
+    mysqli_close($db);
+    include_template("footer"); 
+?>
--- admin/propierties/create.php
+++ admin/propierties/create.php
@@ -4,18 +4,21 @@ require '../../includes/functions.php';
 
 $db = connect_db(); // data base connection
 
-//get data from mysql
-$query = "SELECT * FROM vendedores";
+$query = "SELECT * FROM vendedores"; //get data from mysql
 $result = mysqli_query($db, $query); // this way can present security issues, so it's neccessary to sanitize the data
 
 $errores = []; //errors messages array
 $titulo = $precio = $descripcion = $habitaciones = $wc = $estacionamientos = $vendedorId = "";
 
-//Execute code after submit the form
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    //data sanitization for mysqli
-    $titulo = mysqli_real_escape_string($db, $_POST["titulo"]);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') { //Execute code just after submit the form
+
+    echo "<pre>";
+    var_dump($_POST);
+    echo "</pre>"; 
+
+    $titulo = mysqli_real_escape_string($db, $_POST["titulo"]); //mysql scape is used to sanitize the received data
     $precio = mysqli_real_escape_string($db, $_POST["precio"]);
+    $imagen = $_FILES["imagen"];
     $descripcion = mysqli_real_escape_string($db, $_POST["descripcion"]);
     $habitaciones = mysqli_real_escape_string($db, $_POST["habitaciones"]);
     $wc = mysqli_real_escape_string($db, $_POST["wc"]);
@@ -23,51 +26,64 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $creado = date('Y/m/d');
     $vendedorId = mysqli_real_escape_string($db, $_POST["vendedor"]);
 
-    if (!isset($titulo) || trim($titulo) == '') {
+    if (!isset($titulo) || trim($titulo) == '')
         $errores[] = "Debes Añadir un titulo";
-    }
-    if (!isset($precio) || is_numeric($precio) == '') {
+
+    if (!isset($precio) || is_numeric($precio) == '')
         $errores[] = "Debes ingresar un precio)";
-    }
-    if (!isset($descripcion) || sizeof(explode(" ", $descripcion)) < 20) {
-        $errores[] = "La descripcion debe tener almenos 40 Caracteres";
-    }
-    if (!isset($habitaciones) || trim($habitaciones) == '') {
+
+    if (!isset($descripcion) || sizeof(explode(" ", $descripcion)) < 10)
+        $errores[] = "La descripcion debe tener almenos 10 Caracteres";
+
+    if (!isset($habitaciones) || trim($habitaciones) == '')
         $errores[] = "El número de habitaciones es obligatorio";
-    }
-    if (!isset($wc) || trim($wc) == '') {
+
+    if (!isset($wc) || trim($wc) == '')
         $errores[] = "El número de baños es obligatorio";
-    }
-    if (!isset($estacionamientos) || trim($estacionamientos) == '') {
+
+    if (!isset($estacionamientos) || trim($estacionamientos) == '')
         $errores[] = "El número de habitaciones es obligatorio";
-    }
-    if (!isset($vendedorId) || trim($vendedorId) == '') {
+
+    if (!isset($vendedorId) || trim($vendedorId) == '')
         $errores[] = "Elige un vendedor";
+
+    if (!$imagen["name"] || $imagen["error"]) {
+        $errores[] = "Es necesario subir una imagen";
+        if ($imagen["size"] > 2e6)
+            $errores[] = "El tamaño de la imagen debe ser menor a 2 Mb"; //by default php limit the image size to 2 Mb
     }
 
-    // Insert into database
+    // ---------- Insert data into database------------------
     if (empty($errores)) {
-        $query = "INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, estacionamientos, creado, vendedorId) 
-        VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamientos', '$creado', '$vendedorId')";
+        $dirImages = "../../build/imgPropierties/";
+        if (!is_dir($dirImages)) {
+            mkdir($dirImages); // make dir for propierties images
+        }
+        $imgName = md5(uniqid(rand(), true)) . ".jpg"; //generating unique name
+        move_uploaded_file($imagen["tmp_name"], $dirImages . $imgName); //upload image in the server
+
+        $query = "INSERT INTO propiedades (titulo, precio, imagen, descripcion, habitaciones, wc, estacionamientos, creado, vendedorId) 
+        VALUES ('$titulo', '$precio', '$imgName','$descripcion', '$habitaciones', '$wc', '$estacionamientos', '$creado', '$vendedorId')";
         $result = mysqli_query($db, $query);
 
         if ($result) {
-            header("Location: /admin"); //Reroute user
+            header("Location: /admin?result=1"); //Reroute user and show query string
         }
     }
 }
-include_template("header"); // loading web site header
-?>
+include_template("header"); ?>
+<!-- // loading web site header -->
+
 
 <main class="contenedor seccion">
     <h1>Agragar elementos a la base de datos</h1>
 
-    <!-- List error in site -->
     <?php foreach ($errores as $error) : ?>
+        <!-- List error in site -->
         <div class="alerta error"> <?php echo $error; ?> </div>
     <?php endforeach; ?>
 
-    <form class="formulario" method="POST" action="/admin/propierties/create.php">
+    <form class="formulario" method="POST" action="/admin/propierties/create.php" enctype="multipart/form-data">
         <fieldset>
             <legend>Información General</legend>
             <label for="titulo">Título</label>
@@ -77,7 +93,7 @@ include_template("header"); // loading web site header
             <input type="number" id="precio" name="precio" value="<?php echo $precio ?>" placeholder="Precio. Ej: 120000000">
 
             <label for="imagen">Imagen</label>
-            <input type="file" id="imagen" accept="image/jpeg, image/png">
+            <input type="file" id="imagen" name="imagen" accept="image/jpeg, image/png">
 
             <label for="descripcion">Descripción</label>
             <textarea id="descripcion" name="descripcion"><?php echo $descripcion ?></textarea>
@@ -97,11 +113,11 @@ include_template("header"); // loading web site header
 
         <fieldset>
             <legend>Vendedor</legend>
-            <select name="vendedor" id="vendedor">
+            <select id="vendedor" name="vendedor">
                 <option value="" disabled selected>-- seleccione</option>
                 <?php while ($row = mysqli_fetch_assoc($result)) : ?>
                     <option <?php echo $vendedorId === $row["id"] ? "selected" : "" ?> value="<?php echo $row["id"]; ?>">
-                        <?php echo $row["nombre"]." ".$row["apellido"]; ?>
+                        <?php echo $row["nombre"] . " " . $row["apellido"]; ?>
                     </option>
                 <?php endwhile; ?>
             </select>
